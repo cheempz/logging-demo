b015ec793279f7f01f1796090ded2b1384732e90
Lin Lin <lin@librato.com>
2020-05-01 16:39:26 -0700

    set api token once; update to latest php config file

--- demo.sh
+++ demo.sh
@@ -8,13 +8,13 @@ cleanup() {
 
 trap cleanup SIGINT
 
+export APPOPTICS_API_TOKEN=change-me
 docker-compose down -v --remove-orphans
 docker-compose up -d
 
 sleep 2
 
 while true; do
-    curl -i http://localhost:8001/chain?target=http://php/logging.php
+    curl -i http://localhost:8001/chain?target=http://php-logdemo/logging.php
     sleep 1
 done
-
